198842b0923c6642936f622cf7eb1e50608fcd8e
Stefan <stadolf@gmail.com>
2022-08-01 12:18:09 +0200

    extracts inr/decr functions to a library
    Signed-off-by: Stefan <stadolf@gmail.com>

--- packages/contracts/contracts/Niflot.sol
+++ packages/contracts/contracts/Niflot.sol
@@ -7,6 +7,7 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ISuperfluid, ISuperToken, ISuperApp } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 import { IConstantFlowAgreementV1 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 import { CFAv1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+import { IncDec } from "./IncDec.sol";
 import "hardhat/console.sol";
 
 struct NiflotMetadata {
@@ -20,6 +21,7 @@ struct NiflotMetadata {
 
 contract Niflot is ERC721, Ownable {
     using CFAv1Library for CFAv1Library.InitData;
+    using IncDec for CFAv1Library.InitData;
 
     ISuperfluid private _host;
 
@@ -75,84 +77,6 @@ contract Niflot is ERC721, Ownable {
         _acceptedTokens[token] = accept;
     }
 
-    function _decreaseFlow(
-        ISuperToken token,
-        address to,
-        int96 by
-    ) internal {
-        (, int96 curRate, , ) = _cfa.getFlow(token, address(this), to);
-        int96 newRate = curRate - by;
-        require(newRate >= 0, "new rate would be negative");
-
-        if (newRate == 0) {
-            cfaV1.deleteFlow(address(this), to, token);
-        } else {
-            cfaV1.updateFlow(to, token, newRate);
-        }
-    }
-
-    function _increaseFlow(
-        ISuperToken token,
-        address to,
-        int96 by
-    ) internal {
-        (, int96 curRate, , ) = _cfa.getFlow(token, address(this), to);
-        int96 newRate = curRate + by;
-        require(newRate >= 0, "overflow");
-        if (curRate == 0) {
-            cfaV1.createFlow(to, token, by);
-        } else {
-            cfaV1.updateFlow(to, token, newRate);
-        }
-    }
-
-    function _increaseFlowByOperator(
-        ISuperToken token,
-        address from,
-        address to,
-        int96 by
-    ) internal {
-        (, uint8 permissions, int96 allowance) = _cfa.getFlowOperatorData(
-            token,
-            from,
-            address(this)
-        );
-
-        //always true when full control has been given
-        require(permissions == 7, "origin hasn't permitted Niflot as operator");
-        (, int96 curRate, , ) = _cfa.getFlow(token, from, to);
-
-        int96 newRate = curRate + by;
-        require(newRate >= 0, "overflow");
-        require(
-            newRate < allowance,
-            "origin doesn't allow us to allocate that flowrate"
-        );
-
-        if (curRate == 0) {
-            cfaV1.createFlowByOperator(from, to, token, by);
-        } else {
-            cfaV1.updateFlowByOperator(from, to, token, newRate);
-        }
-    }
-
-    function _decreaseFlowByOperator(
-        ISuperToken token,
-        address from,
-        address to,
-        int96 by
-    ) internal {
-        (, int96 curRate, , ) = _cfa.getFlow(token, from, to);
-        int96 newRate = curRate - by;
-        require(newRate >= 0, "new rate would be negative");
-
-        if (newRate == 0) {
-            cfaV1.deleteFlowByOperator(from, to, token);
-        } else {
-            cfaV1.updateFlowByOperator(from, to, token, newRate);
-        }
-    }
-
     function mint(
         ISuperToken token,
         address origin,
@@ -164,7 +88,13 @@ contract Niflot is ERC721, Ownable {
 
         //delete original stream
         cfaV1.deleteFlowByOperator(origin, msg.sender, token);
-        _increaseFlowByOperator(token, origin, address(this), flowrate);
+        cfaV1._increaseFlowByOperator(
+            _cfa,
+            token,
+            origin,
+            address(this),
+            flowrate
+        );
 
         niflots[nextId] = NiflotMetadata({
             origin: origin,
@@ -200,14 +130,16 @@ contract Niflot is ERC721, Ownable {
     function _recoverOriginalFlow(uint256 tokenId) internal {
         NiflotMetadata memory meta = niflots[tokenId];
         //reduce flowrate from operator to niflot
-        _decreaseFlowByOperator(
+        cfaV1._decreaseFlowByOperator(
+            _cfa,
             meta.token,
             meta.origin,
             address(this),
             meta.flowrate
         );
         //reinstantiate original flow
-        _increaseFlowByOperator(
+        cfaV1._increaseFlowByOperator(
+            _cfa,
             meta.token,
             meta.origin,
             meta.receiver,
@@ -230,10 +162,10 @@ contract Niflot is ERC721, Ownable {
         NiflotMetadata memory meta = niflots[tokenId];
         if (oldReceiver == address(0)) {
             //minted
-            _increaseFlow(meta.token, newReceiver, meta.flowrate);
+            cfaV1._increaseFlow(_cfa, meta.token, newReceiver, meta.flowrate);
         } else if (newReceiver == address(0)) {
             //burnt
-            _decreaseFlow(meta.token, oldReceiver, meta.flowrate);
+            cfaV1._decreaseFlow(_cfa, meta.token, oldReceiver, meta.flowrate);
             _recoverOriginalFlow(tokenId);
             delete niflots[tokenId];
         } else {
@@ -255,8 +187,8 @@ contract Niflot is ERC721, Ownable {
             }
 
             //handover flow
-            _decreaseFlow(meta.token, oldReceiver, meta.flowrate);
-            _increaseFlow(meta.token, newReceiver, meta.flowrate);
+            cfaV1._decreaseFlow(_cfa, meta.token, oldReceiver, meta.flowrate);
+            cfaV1._increaseFlow(_cfa, meta.token, newReceiver, meta.flowrate);
         }
     }
 
